0d7860d7e0fd9ef26752e68290c9aad9a19a9e8e
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2020-11-10 17:37:58 +0100

    Cleanup code

--- src/main/java/de/peass/ci/MeasureVersionBuilder.java
+++ src/main/java/de/peass/ci/MeasureVersionBuilder.java
@@ -14,7 +14,6 @@ import javax.servlet.ServletException;
 import javax.xml.bind.JAXBException;
 
 import org.apache.commons.io.FileUtils;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
@@ -28,17 +27,8 @@ import de.dagere.kopeme.generated.TestcaseType.Datacollector.Chunk;
 import de.peass.analysis.changes.Change;
 import de.peass.analysis.changes.Changes;
 import de.peass.analysis.changes.ProjectChanges;
-import de.peass.dependency.CauseSearchFolders;
 import de.peass.dependency.PeASSFolders;
-import de.peass.dependency.analysis.data.TestCase;
 import de.peass.dependency.execution.MeasurementConfiguration;
-import de.peass.dependencyprocessors.ViewNotFoundException;
-import de.peass.measurement.rca.CauseSearcher;
-import de.peass.measurement.rca.CauseSearcherComplete;
-import de.peass.measurement.rca.CauseSearcherConfig;
-import de.peass.measurement.rca.CauseTester;
-import de.peass.measurement.rca.kieker.BothTreeReader;
-import de.peass.testtransformation.JUnitTestTransformer;
 import de.peass.utils.Constants;
 import de.peass.visualization.VisualizeRCA;
 import de.peran.measurement.analysis.ProjectStatistics;
@@ -53,7 +43,6 @@ import hudson.tasks.Builder;
 import hudson.util.FormValidation;
 import io.jenkins.cli.shaded.org.apache.commons.io.filefilter.WildcardFileFilter;
 import jenkins.tasks.SimpleBuildStep;
-import kieker.analysis.exception.AnalysisConfigurationException;
 
 public class MeasureVersionBuilder extends Builder implements SimpleBuildStep {
 
@@ -74,65 +63,74 @@ public class MeasureVersionBuilder extends Builder implements SimpleBuildStep {
 
    @Override
    public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
-      final MeasurementConfiguration config = getConfig();
-
-      Map<String, HistogramValues> measurements = new TreeMap<>();
       if (!workspace.exists()) {
-         listener.getLogger().println("Workspace folder " + workspace.toString() + " does not exist, please asure that the repository was correctly cloned!");
+         throw new RuntimeException("Workspace folder " + workspace.toString() + " does not exist, please asure that the repository was correctly cloned!");
       } else {
          listener.getLogger().println("Executing on " + workspace.toString());
+
          PrintStream outOriginal = System.out;
          PrintStream errOriginal = System.err;
-         final File projectFolder = new File(workspace.toString());
-         final ContinuousExecutor executor = new ContinuousExecutor(projectFolder, config, 1, true);
-         ProjectChanges changes = null;
+
          try {
             System.setOut(listener.getLogger());
             System.setErr(listener.getLogger());
 
-            executor.execute();
+            final MeasurementConfiguration measurementConfig = getConfig();
             
-            File measurementsFullFolder = executor.getFullResultsVersion();
-            for (File xmlResultFile : measurementsFullFolder.listFiles((FileFilter) new WildcardFileFilter("*.xml"))) {
-               Kopemedata data = XMLDataLoader.loadData(xmlResultFile);
-               //This assumes measurements are only executed once; if this is not the case, the matching result would need to be searched
-               final TestcaseType testcase = data.getTestcases().getTestcase().get(0);
-               HistogramValues values = getHistogramValues(executor, testcase);
-               measurements.put(data.getTestcases().getClazz() + "#" + testcase.getName(), values);
-            }
+            final File projectFolder = new File(workspace.toString());
+            final ContinuousExecutor executor = new ContinuousExecutor(projectFolder, measurementConfig, 1, true);
+            executor.execute();
 
-            changes = Constants.OBJECTMAPPER.readValue(new File(executor.getLocalFolder(), "changes.json"), ProjectChanges.class);
+            Map<String, HistogramValues> measurements =  readMeasurements(executor);
 
-            executeRCAs(config, executor, changes);
+            final ProjectChanges changes = Constants.OBJECTMAPPER.readValue(new File(executor.getLocalFolder(), "changes.json"), ProjectChanges.class);
+
+            RCAExecutor rcaexecutor = new RCAExecutor(measurementConfig, executor, changes);
+            rcaexecutor.executeRCAs();
 
             visualizeRCA(executor, changes, run);
+
+            ProjectStatistics statistics = Constants.OBJECTMAPPER.readValue(new File(executor.getLocalFolder(), "statistics.json"), ProjectStatistics.class);
+
+            final MeasureVersionAction action = new MeasureVersionAction(measurementConfig, changes, statistics, measurements);
+            run.addAction(action);
          } catch (Throwable e) {
             e.printStackTrace();
          } finally {
             System.setOut(outOriginal);
             System.setErr(errOriginal);
          }
-         ProjectStatistics statistics = Constants.OBJECTMAPPER.readValue(new File(executor.getLocalFolder(), "statistics.json"), ProjectStatistics.class);
 
-         final MeasureVersionAction action = new MeasureVersionAction(config, changes, statistics, measurements);
-         run.addAction(action);
       }
+   }
+
+   private Map<String, HistogramValues> readMeasurements(final ContinuousExecutor executor) throws JAXBException {
+      Map<String, HistogramValues> measurements = new TreeMap<>(); 
+      File measurementsFullFolder = executor.getFullResultsVersion();
+      for (File xmlResultFile : measurementsFullFolder.listFiles((FileFilter) new WildcardFileFilter("*.xml"))) {
+         Kopemedata data = XMLDataLoader.loadData(xmlResultFile);
+         // This assumes measurements are only executed once; if this is not the case, the matching result would need to be searched
+         final TestcaseType testcase = data.getTestcases().getTestcase().get(0);
+         HistogramValues values = getHistogramValues(executor, testcase);
+         measurements.put(data.getTestcases().getClazz() + "#" + testcase.getName(), values);
+      }
+      return measurements;
    }
 
    private HistogramValues getHistogramValues(final ContinuousExecutor executor, final TestcaseType testcase) {
       Chunk chunk = testcase.getDatacollector().get(0).getChunk().get(0);
-      
+
       List<Double> old = new LinkedList<>();
       List<Double> current = new LinkedList<>();
       for (Result result : chunk.getResult()) {
-         if (result.getVersion().getGitversion().equals(executor.getVersionOld())){
+         if (result.getVersion().getGitversion().equals(executor.getVersionOld())) {
             old.add(result.getValue());
          }
          if (result.getVersion().getGitversion().equals(executor.getLatestVersion())) {
             current.add(result.getValue());
          }
       }
-      HistogramValues values = new HistogramValues(current.stream().mapToDouble(i -> i).toArray(), 
+      HistogramValues values = new HistogramValues(current.stream().mapToDouble(i -> i).toArray(),
             old.stream().mapToDouble(i -> i).toArray());
       return values;
    }
@@ -175,43 +173,6 @@ public class MeasureVersionBuilder extends Builder implements SimpleBuildStep {
             }
          }
       }
-   }
-
-   private void executeRCAs(final MeasurementConfiguration config, final ContinuousExecutor executor, ProjectChanges changes)
-         throws IOException, InterruptedException, XmlPullParserException, AnalysisConfigurationException, ViewNotFoundException, JAXBException {
-      config.setVersion(executor.getLatestVersion());
-      config.setVersionOld(executor.getVersionOld());
-
-      Changes versionChanges = changes.getVersion(executor.getLatestVersion());
-      for (Entry<String, List<Change>> testcases : versionChanges.getTestcaseChanges().entrySet()) {
-         for (Change change : testcases.getValue()) {
-            CauseSearchFolders folders = new CauseSearchFolders(executor.getProjectFolder());
-
-            String onlyTestcaseName = (testcases.getKey().contains(".") ? testcases.getKey().substring(testcases.getKey().lastIndexOf('.') + 1) : testcases.getKey());
-            final File expectedResultFile = new File(folders.getRcaTreeFolder(),
-                  executor.getLatestVersion() + File.separator +
-                        onlyTestcaseName + File.separator +
-                        change.getMethod() + ".json");
-            System.out.println("Testing " + expectedResultFile);
-            if (!expectedResultFile.exists()) {
-               executeRCA(config, executor, testcases, change);
-            }
-         }
-      }
-   }
-
-   private void executeRCA(final MeasurementConfiguration config, final ContinuousExecutor executor, Entry<String, List<Change>> testcases, Change change)
-         throws IOException, InterruptedException, XmlPullParserException, AnalysisConfigurationException, ViewNotFoundException, JAXBException {
-      final TestCase testCase = new TestCase(testcases.getKey() + "#" + change.getMethod());
-      final CauseSearcherConfig causeSearcherConfig = new CauseSearcherConfig(testCase, true, true, 5.0, true, 0.01, false, true);
-      config.setUseKieker(true);
-
-      final CauseSearchFolders alternateFolders = new CauseSearchFolders(executor.getFolders().getProjectFolder());
-      final JUnitTestTransformer testtransformer = new JUnitTestTransformer(executor.getFolders().getProjectFolder(), config);
-      final BothTreeReader reader = new BothTreeReader(causeSearcherConfig, config, alternateFolders);
-      final CauseTester measurer = new CauseTester(alternateFolders, testtransformer, causeSearcherConfig);
-      final CauseSearcher tester = new CauseSearcherComplete(reader, causeSearcherConfig, measurer, config, alternateFolders);
-      tester.search();
    }
 
    private MeasurementConfiguration getConfig() {
---
+++ src/main/java/de/peass/ci/RCAExecutor.java
@@ -0,0 +1,73 @@
+package de.peass.ci;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map.Entry;
+
+import javax.xml.bind.JAXBException;
+
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import de.peass.analysis.changes.Change;
+import de.peass.analysis.changes.Changes;
+import de.peass.analysis.changes.ProjectChanges;
+import de.peass.dependency.CauseSearchFolders;
+import de.peass.dependency.analysis.data.TestCase;
+import de.peass.dependency.execution.MeasurementConfiguration;
+import de.peass.dependencyprocessors.ViewNotFoundException;
+import de.peass.measurement.rca.CauseSearcher;
+import de.peass.measurement.rca.CauseSearcherComplete;
+import de.peass.measurement.rca.CauseSearcherConfig;
+import de.peass.measurement.rca.CauseTester;
+import de.peass.measurement.rca.kieker.BothTreeReader;
+import de.peass.testtransformation.JUnitTestTransformer;
+import kieker.analysis.exception.AnalysisConfigurationException;
+
+class RCAExecutor{
+   final MeasurementConfiguration config;
+   final ContinuousExecutor executor;
+   final ProjectChanges changes;
+   public RCAExecutor(MeasurementConfiguration config, ContinuousExecutor executor, ProjectChanges changes) {
+      this.config = config;
+      this.executor = executor;
+      this.changes = changes;
+   }
+   
+   public void executeRCAs()
+         throws IOException, InterruptedException, XmlPullParserException, AnalysisConfigurationException, ViewNotFoundException, JAXBException {
+      config.setVersion(executor.getLatestVersion());
+      config.setVersionOld(executor.getVersionOld());
+
+      Changes versionChanges = changes.getVersion(executor.getLatestVersion());
+      for (Entry<String, List<Change>> testcases : versionChanges.getTestcaseChanges().entrySet()) {
+         for (Change change : testcases.getValue()) {
+            CauseSearchFolders folders = new CauseSearchFolders(executor.getProjectFolder());
+
+            String onlyTestcaseName = (testcases.getKey().contains(".") ? testcases.getKey().substring(testcases.getKey().lastIndexOf('.') + 1) : testcases.getKey());
+            final File expectedResultFile = new File(folders.getRcaTreeFolder(),
+                  executor.getLatestVersion() + File.separator +
+                        onlyTestcaseName + File.separator +
+                        change.getMethod() + ".json");
+            System.out.println("Testing " + expectedResultFile);
+            if (!expectedResultFile.exists()) {
+               executeRCA(config, executor, testcases, change);
+            }
+         }
+      }
+   }
+   
+   private void executeRCA(final MeasurementConfiguration config, final ContinuousExecutor executor, Entry<String, List<Change>> testcases, Change change)
+         throws IOException, InterruptedException, XmlPullParserException, AnalysisConfigurationException, ViewNotFoundException, JAXBException {
+      final TestCase testCase = new TestCase(testcases.getKey() + "#" + change.getMethod());
+      final CauseSearcherConfig causeSearcherConfig = new CauseSearcherConfig(testCase, true, true, 5.0, true, 0.01, false, true);
+      config.setUseKieker(true);
+
+      final CauseSearchFolders alternateFolders = new CauseSearchFolders(executor.getFolders().getProjectFolder());
+      final JUnitTestTransformer testtransformer = new JUnitTestTransformer(executor.getFolders().getProjectFolder(), config);
+      final BothTreeReader reader = new BothTreeReader(causeSearcherConfig, config, alternateFolders);
+      final CauseTester measurer = new CauseTester(alternateFolders, testtransformer, causeSearcherConfig);
+      final CauseSearcher tester = new CauseSearcherComplete(reader, causeSearcherConfig, measurer, config, alternateFolders);
+      tester.search();
+   }
+}
