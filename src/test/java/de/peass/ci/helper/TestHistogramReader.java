f5f7548ad0dfa81bb107b1d65f38814d44220e47
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2020-11-16 20:41:15 +0100

    Add histogram reader test

---
+++ src/test/java/de/peass/ci/helper/TestHistogramReader.java
@@ -0,0 +1,31 @@
+package de.peass.ci.helper;
+
+import java.io.File;
+import java.util.Map;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import de.peass.ci.ContinuousExecutor;
+
+public class TestHistogramReader {
+   
+   @Test
+   public void testHistogramCreation() throws JAXBException {
+      ContinuousExecutor executorMock = Mockito.mock(ContinuousExecutor.class);
+      Mockito.when(executorMock.getLatestVersion()).thenReturn("b02c92af73e3297be617f4c973a7a63fb603565b");
+      Mockito.when(executorMock.getVersionOld()).thenReturn("e80d8a1bf747d1f70dc52260616b36cac9e44561");
+      Mockito.when(executorMock.getFullResultsVersion()).thenReturn(new File("src/test/resources/demo-results/histogram/b02c92af73e3297be617f4c973a7a63fb603565b"));
+      
+      HistogramReader reader = new HistogramReader(executorMock);
+      Map<String, HistogramValues> measurements = reader.readMeasurements();
+      
+      System.out.println(measurements.keySet());
+      
+      Assert.assertEquals(measurements.get("de.test.CalleeTest#onlyCallMethod1").getValuesBefore().length, 2);
+      Assert.assertEquals(measurements.get("de.test.CalleeTest#onlyCallMethod1").getValuesCurrent().length, 2);
+   }
+}
