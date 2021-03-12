28bdd1a34ad46df8f0850348c18fd28a48f2fc39
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-03-12 10:22:05 +0100

    Added SnapshotDependencyChecker

--- src/main/java/de/peass/ci/remote/RemoteMeasurer.java
+++ src/main/java/de/peass/ci/remote/RemoteMeasurer.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jenkinsci.remoting.RoleChecker;
 
 import de.peass.ci.ContinuousExecutor;
@@ -18,10 +16,6 @@ import hudson.remoting.VirtualChannel;
 public class RemoteMeasurer implements FileCallable<Boolean> {
 
    private static final long serialVersionUID = 5145199366806250594L;
-
-   private static final Logger LOG = LogManager.getLogger();
-
-   private static final String seperator = File.separator;
 
    private final MeasurementConfiguration measurementConfig;
 
@@ -45,7 +39,7 @@ public class RemoteMeasurer implements FileCallable<Boolean> {
          /*
           * This is just a workaround until all dependencies are available in maven central repository.
           */
-         checkKopemeAndKieker(workspaceFolder);
+         SnapshotDependencyChecker.checkKopemeAndKieker(workspaceFolder);
 
          final ContinuousExecutor executor = new ContinuousExecutor(workspaceFolder, measurementConfig, 1, true);
          executor.execute();
@@ -60,55 +54,6 @@ public class RemoteMeasurer implements FileCallable<Boolean> {
          e.printStackTrace();
          return false;
       }
-   }
-
-   private void checkKopemeAndKieker(final File workspaceFolder) throws InterruptedException, IOException {
-
-      final String home = System.getProperty("user.home");
-
-      final String mavenRepo = home + seperator + ".m2" + seperator + "repository" + seperator;
-
-      final File kopeme = new File(mavenRepo + "de" + seperator + "dagere" + seperator + "kopeme" + seperator + "kopeme-junit" + seperator + "0.14-SNAPSHOT");
-      final File kieker = new File(mavenRepo + "net" + seperator + "kieker-monitoring" + seperator + "kieker" + seperator + "1.15-SNAPSHOT");
-
-      if (!kopemeAndKiekerExist(kopeme, kieker)) {
-         cloneAndinstallPeass(workspaceFolder);
-      }
-
-      if (!kopemeAndKiekerExist(kopeme, kieker)) {
-         LOG.warn("Kopeme and/or Kieker dependencies could still not be found! Build will possibly fail!");
-      }
-   }
-
-   private boolean kopemeAndKiekerExist(final File kopeme, final File kieker) {
-      return kopeme.exists() && kieker.exists();
-   }
-
-   private void cloneAndinstallPeass(final File workspaceFolder) throws InterruptedException, IOException {
-      LOG.warn("Kopeme and/or Kieker dependencies could not be found. Installing peass.");
-
-      clonePeass(workspaceFolder);
-      installPeass(workspaceFolder);
-   }
-
-   private void clonePeass(final File workspaceFolder) throws InterruptedException, IOException {
-      final File logFile = new File(workspaceFolder, "clonePeassLog.txt");
-      LOG.info("Cloning peass. Log goes to {}", logFile.getAbsolutePath());
-
-      final ProcessBuilder builder = new ProcessBuilder("git", "clone", "--progress", "https://github.com/DaGeRe/peass")
-            .directory(new File(workspaceFolder.getAbsolutePath() + seperator + ".."))
-            .redirectError(logFile);
-      builder.start().waitFor();
-   }
-
-   private void installPeass(final File workspaceFolder) throws InterruptedException, IOException {
-      final File logFile = new File(workspaceFolder, "installPeassLog.txt");
-      LOG.info("Installing peass. Log goes to {}", logFile.getAbsolutePath());
-
-      final ProcessBuilder builder = new ProcessBuilder("mvn", "install", "-DskipTests")
-            .directory(new File(workspaceFolder.getAbsolutePath() + seperator + ".." + seperator + "peass"))
-            .redirectOutput(logFile);
-      builder.start().waitFor();
    }
 
 }
---
+++ src/main/java/de/peass/ci/remote/SnapshotDependencyChecker.java
@@ -0,0 +1,61 @@
+package de.peass.ci.remote;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SnapshotDependencyChecker {
+   
+   private static final Logger LOG = LogManager.getLogger();
+   private static final String seperator = File.separator;
+   
+   protected static void checkKopemeAndKieker(final File workspaceFolder) throws InterruptedException, IOException {
+
+      final String home = System.getProperty("user.home");
+      final String mavenRepo = home + seperator + ".m2" + seperator + "repository" + seperator;
+
+      final File kopeme = new File(mavenRepo + "de" + seperator + "dagere" + seperator + "kopeme" + seperator + "kopeme-junit" + seperator + "0.14-SNAPSHOT");
+      final File kieker = new File(mavenRepo + "net" + seperator + "kieker-monitoring" + seperator + "kieker" + seperator + "1.15-SNAPSHOT");
+
+      if (!kopemeAndKiekerExist(kopeme, kieker)) {
+         cloneAndinstallPeass(workspaceFolder);
+      }
+
+      if (!kopemeAndKiekerExist(kopeme, kieker)) {
+         LOG.warn("Kopeme and/or Kieker dependencies could still not be found! Build will possibly fail!");
+      }
+   }
+
+   private static boolean kopemeAndKiekerExist(final File kopeme, final File kieker) {
+      return kopeme.exists() && kieker.exists();
+   }
+
+   private static void cloneAndinstallPeass(final File workspaceFolder) throws InterruptedException, IOException {
+      LOG.warn("Kopeme and/or Kieker dependencies could not be found. Installing peass.");
+      clonePeass(workspaceFolder);
+      installPeass(workspaceFolder);
+   }
+
+   private static void clonePeass(final File workspaceFolder) throws InterruptedException, IOException {
+      final File logFile = new File(workspaceFolder, "clonePeassLog.txt");
+      LOG.info("Cloning peass. Log goes to {}", logFile.getAbsolutePath());
+
+      final ProcessBuilder builder = new ProcessBuilder("git", "clone", "--progress", "https://github.com/DaGeRe/peass")
+            .directory(new File(workspaceFolder.getAbsolutePath() + seperator + ".."))
+            .redirectError(logFile);
+      builder.start().waitFor();
+   }
+
+   private static void installPeass(final File workspaceFolder) throws InterruptedException, IOException {
+      final File logFile = new File(workspaceFolder, "installPeassLog.txt");
+      LOG.info("Installing peass. Log goes to {}", logFile.getAbsolutePath());
+
+      final ProcessBuilder builder = new ProcessBuilder("mvn", "install", "-DskipTests")
+            .directory(new File(workspaceFolder.getAbsolutePath() + seperator + ".." + seperator + "peass"))
+            .redirectOutput(logFile);
+      builder.start().waitFor();
+   }
+
+}
