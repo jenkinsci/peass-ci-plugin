cc54415dc0587765bb46265d8d0a8e99a2b25a05
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-08-24 15:20:10 +0200

    Extract Action Creators for measurement and RCA

--- src/main/java/de/dagere/peass/ci/logs/LogActionCreator.java
+++ src/main/java/de/dagere/peass/ci/logs/LogActionCreator.java
@@ -2,21 +2,12 @@ package de.dagere.peass.ci.logs;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.io.FileUtils;
 
 import de.dagere.peass.ci.PeassProcessConfiguration;
 import de.dagere.peass.ci.helper.VisualizationFolderManager;
-import de.dagere.peass.ci.logs.measurement.LogAction;
-import de.dagere.peass.ci.logs.measurement.LogOverviewAction;
-import de.dagere.peass.ci.logs.rca.RCALevel;
-import de.dagere.peass.ci.logs.rca.RCALogAction;
-import de.dagere.peass.ci.logs.rca.RCALogOverviewAction;
+import de.dagere.peass.ci.logs.measurement.MeasurementActionCreator;
+import de.dagere.peass.ci.logs.rca.RCAActionCreator;
 import de.dagere.peass.ci.logs.rts.RTSActionCreator;
-import de.dagere.peass.dependency.analysis.data.TestCase;
 import de.dagere.peass.measurement.analysis.ProjectStatistics;
 import hudson.model.Run;
 
@@ -39,56 +30,12 @@ public class LogActionCreator {
    }
 
    public void createMeasurementActions(final ProjectStatistics statistics) throws IOException {
-      Map<TestCase, List<LogFiles>> logFiles = reader.readAllTestcases(statistics);
-      createLogActions(run, logFiles);
-      
-      String measureLog = reader.getMeasureLog();
-      run.addAction(new InternalLogAction("measurementLog", "Measurement Log", measureLog));
-      
-      LogOverviewAction logOverviewAction = new LogOverviewAction(logFiles, peassConfig.getMeasurementConfig().getVersion().substring(0,6), peassConfig.getMeasurementConfig().getVersionOld().substring(0,6));
-      run.addAction(logOverviewAction);
-   }
-   
-   private void createLogActions(final Run<?, ?> run, final Map<TestCase, List<LogFiles>> logFiles) throws IOException {
-      for (Map.Entry<TestCase, List<LogFiles>> entry : logFiles.entrySet()) {
-         TestCase testcase = entry.getKey();
-         int vmId = 0;
-         for (LogFiles files : entry.getValue()) {
-            String logData = FileUtils.readFileToString(files.getCurrent(), StandardCharsets.UTF_8);
-            run.addAction(new LogAction(testcase, vmId, peassConfig.getMeasurementConfig().getVersion(), logData));
-            String logDataOld = FileUtils.readFileToString(files.getPredecessor(), StandardCharsets.UTF_8);
-            run.addAction(new LogAction(testcase, vmId, peassConfig.getMeasurementConfig().getVersionOld(), logDataOld));
-            vmId++;
-         }
-      }
+      MeasurementActionCreator measurementActionCreator = new MeasurementActionCreator(reader, run, peassConfig.getMeasurementConfig());
+      measurementActionCreator.createMeasurementActions(statistics);
    }
    
    public void createRCAActions() throws IOException {
-      String rcaLog = reader.getRCALog();
-      run.addAction(new InternalLogAction("rcaLog", "RCA Log", rcaLog));
-      
-      Map<TestCase, List<RCALevel>> testLevelMap = createRCALogActions(reader);
-      
-      RCALogOverviewAction rcaOverviewAction = new RCALogOverviewAction(testLevelMap, peassConfig.getMeasurementConfig().getVersion().substring(0,6), peassConfig.getMeasurementConfig().getVersionOld().substring(0,6));
-      run.addAction(rcaOverviewAction);
-   }
-
-   private Map<TestCase, List<RCALevel>> createRCALogActions(final LogFileReader reader) throws IOException {
-      Map<TestCase, List<RCALevel>> testLevelMap = reader.getRCATestcases();
-      for (Map.Entry<TestCase, List<RCALevel>> testcase : testLevelMap.entrySet()) {
-         int levelId = 0;
-         for (RCALevel level : testcase.getValue()) {
-            int vmId = 0;
-            for (LogFiles files : level.getLogFiles()) {
-               String logData = FileUtils.readFileToString(files.getCurrent(), StandardCharsets.UTF_8);
-               run.addAction(new RCALogAction(testcase.getKey(), vmId, levelId, peassConfig.getMeasurementConfig().getVersion(), logData));
-               String logDataOld = FileUtils.readFileToString(files.getPredecessor(), StandardCharsets.UTF_8);
-               run.addAction(new RCALogAction(testcase.getKey(), vmId, levelId, peassConfig.getMeasurementConfig().getVersionOld(), logDataOld));
-               vmId++;
-            }
-            levelId++;
-         }
-      }
-      return testLevelMap;
+      RCAActionCreator rcaActionCreator = new RCAActionCreator(reader, run, peassConfig.getMeasurementConfig());
+      rcaActionCreator.createRCAActions();
    }
 }
---
+++ src/main/java/de/dagere/peass/ci/logs/rca/RCAActionCreator.java
@@ -0,0 +1,57 @@
+package de.dagere.peass.ci.logs.rca;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+
+import de.dagere.peass.ci.logs.InternalLogAction;
+import de.dagere.peass.ci.logs.LogFileReader;
+import de.dagere.peass.ci.logs.LogFiles;
+import de.dagere.peass.config.MeasurementConfiguration;
+import de.dagere.peass.dependency.analysis.data.TestCase;
+import hudson.model.Run;
+
+public class RCAActionCreator {
+   private final LogFileReader reader;
+   private final Run<?, ?> run;
+   private final MeasurementConfiguration measurementConfig;
+
+   public RCAActionCreator(final LogFileReader reader, final Run<?, ?> run, final MeasurementConfiguration measurementConfig) {
+      this.reader = reader;
+      this.run = run;
+      this.measurementConfig = measurementConfig;
+   }
+
+   public void createRCAActions() throws IOException {
+      String rcaLog = reader.getRCALog();
+      run.addAction(new InternalLogAction("rcaLog", "RCA Log", rcaLog));
+
+      Map<TestCase, List<RCALevel>> testLevelMap = createRCALogActions(reader);
+
+      RCALogOverviewAction rcaOverviewAction = new RCALogOverviewAction(testLevelMap, measurementConfig.getVersion().substring(0, 6),
+            measurementConfig.getVersionOld().substring(0, 6));
+      run.addAction(rcaOverviewAction);
+   }
+
+   private Map<TestCase, List<RCALevel>> createRCALogActions(final LogFileReader reader) throws IOException {
+      Map<TestCase, List<RCALevel>> testLevelMap = reader.getRCATestcases();
+      for (Map.Entry<TestCase, List<RCALevel>> testcase : testLevelMap.entrySet()) {
+         int levelId = 0;
+         for (RCALevel level : testcase.getValue()) {
+            int vmId = 0;
+            for (LogFiles files : level.getLogFiles()) {
+               String logData = FileUtils.readFileToString(files.getCurrent(), StandardCharsets.UTF_8);
+               run.addAction(new RCALogAction(testcase.getKey(), vmId, levelId, measurementConfig.getVersion(), logData));
+               String logDataOld = FileUtils.readFileToString(files.getPredecessor(), StandardCharsets.UTF_8);
+               run.addAction(new RCALogAction(testcase.getKey(), vmId, levelId, measurementConfig.getVersionOld(), logDataOld));
+               vmId++;
+            }
+            levelId++;
+         }
+      }
+      return testLevelMap;
+   }
+}
