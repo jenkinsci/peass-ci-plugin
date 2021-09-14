095ed14bd60cbc9901796b7eeb5827e125c08d63
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-09-14 15:10:24 +0200

    Fix data displaying

--- src/test/java/de/peass/ci/helper/TestHistogramReader.java
+++ src/test/java/de/peass/ci/helper/TestHistogramReader.java
@@ -14,6 +14,8 @@ import de.dagere.peass.config.MeasurementConfiguration;
 
 public class TestHistogramReader {
    
+   private static final File EXAMPLE_DATA_FOLDER = new File("src/test/resources/demo-results/histogram");
+   
    @Test
    public void testHistogramCreation() throws JAXBException {
       MeasurementConfiguration measurementConfig = new MeasurementConfiguration(2);
@@ -23,7 +25,7 @@ public class TestHistogramReader {
       measurementConfig.setIterations(2);
       measurementConfig.setRepetitions(2);
       
-      HistogramReader reader = new HistogramReader(measurementConfig, new File("src/test/resources/demo-results/histogram/b02c92af73e3297be617f4c973a7a63fb603565b"));
+      HistogramReader reader = new HistogramReader(measurementConfig, new File(EXAMPLE_DATA_FOLDER, "b02c92af73e3297be617f4c973a7a63fb603565b"));
       Map<String, HistogramValues> measurements = reader.readMeasurements();
       
       System.out.println(measurements.keySet());
@@ -40,7 +42,7 @@ public class TestHistogramReader {
       measurementConfig.setVersion("e80d8a1bf747d1f70dc52260616b36cac9e44561");
       measurementConfig.setVersionOld("e80d8a1bf747d1f70dc52260616b36cac9e44561~1");
       
-      HistogramReader reader = new HistogramReader(measurementConfig, new File("src/test/resources/demo-results/histogram/e80d8a1bf747d1f70dc52260616b36cac9e44561"));
+      HistogramReader reader = new HistogramReader(measurementConfig, new File(EXAMPLE_DATA_FOLDER, "e80d8a1bf747d1f70dc52260616b36cac9e44561"));
       Map<String, HistogramValues> measurements = reader.readMeasurements();
       
       Assert.assertNull(measurements.get("e80d8a1bf747d1f70dc52260616b36cac9e44561"));
@@ -54,7 +56,7 @@ public class TestHistogramReader {
       measurementConfig.setVersion("a23e385264c31def8dcda86c3cf64faa698c62d8");
       measurementConfig.setVersionOld("33ce17c04b5218c25c40137d4d09f40fbb3e4f0f");
       
-      HistogramReader reader = new HistogramReader(measurementConfig, new File("src/test/resources/demo-results/histogram/measurement_a23e385264c31def8dcda86c3cf64faa698c62d8_33ce17c04b5218c25c40137d4d09f40fbb3e4f0f"));
+      HistogramReader reader = new HistogramReader(measurementConfig, new File(EXAMPLE_DATA_FOLDER, "measurement_a23e385264c31def8dcda86c3cf64faa698c62d8_33ce17c04b5218c25c40137d4d09f40fbb3e4f0f"));
       Map<String, HistogramValues> measurements = reader.readMeasurements();
       
       System.out.println(measurements);
