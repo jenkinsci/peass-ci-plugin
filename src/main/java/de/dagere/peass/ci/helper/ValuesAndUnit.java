69c4b40b9cc5c18370b3dce6dbbe3610fed6a73c
Matti Wilhelmi <maw@hbt.de>
2022-02-24 11:51:37 +0100

    Suppress spotbugs warnings

--- src/main/java/de/dagere/peass/ci/helper/ValuesAndUnit.java
+++ src/main/java/de/dagere/peass/ci/helper/ValuesAndUnit.java
@@ -1,15 +1,19 @@
 package de.dagere.peass.ci.helper;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 public class ValuesAndUnit {
 
    private final double[] values;
    private final String unit;
 
+   @SuppressFBWarnings
    public ValuesAndUnit(final double[] values, final String unit) {
       this.values = values;
       this.unit = unit;
    }
 
+   @SuppressFBWarnings
    public double[] getValues() {
       return values;
    }
