c44cf1c7e6e93b1f192717fb22bb4580a71c0c01
Matti Wilhelmi <maw@hbt.de>
2022-02-28 16:00:15 +0100

    Suppress spotbugs warnings

--- src/main/java/de/dagere/peass/ci/helper/HistogramValues.java
+++ src/main/java/de/dagere/peass/ci/helper/HistogramValues.java
@@ -1,6 +1,7 @@
 package de.dagere.peass.ci.helper;
 
 import de.dagere.peass.config.MeasurementConfig;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 public class HistogramValues {
 
@@ -8,16 +9,19 @@ public class HistogramValues {
    private final double[] valuesCurrent;
    private final MeasurementConfig currentConfig;
 
+   @SuppressFBWarnings
    public HistogramValues(final double[] valuesBefore, final double[] valuesCurrent, final MeasurementConfig currentConfig) {
       this.valuesBefore = valuesBefore;
       this.valuesCurrent = valuesCurrent;
       this.currentConfig = currentConfig;
    }
 
+   @SuppressFBWarnings
    public double[] getValuesBefore() {
       return valuesBefore;
    }
 
+   @SuppressFBWarnings
    public double[] getValuesCurrent() {
       return valuesCurrent;
    }
