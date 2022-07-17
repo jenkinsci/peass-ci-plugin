3c83934233bc18608e1ab4635ae5d9f1af84a5ea
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-07-17 17:45:13 +0200

    Show multiple commits if they happened within the given timeframe

--- src/main/java/de/dagere/peass/ci/peassOverview/ProjectData.java
+++ src/main/java/de/dagere/peass/ci/peassOverview/ProjectData.java
@@ -43,46 +43,61 @@ public class ProjectData {
    public List<ChangeLine> getChangeLines() {
       List<ChangeLine> result = new LinkedList<>();
       if (selection != null) {
-         String commit = selection.getNewestCommit();
-         CommitStaticSelection versionStaticSelection = selection.getCommits().get(commit);
-         if (versionStaticSelection != null) {
-            Map<ChangedEntity, TestSet> changedClazzes = versionStaticSelection.getChangedClazzes();
-            for (Map.Entry<ChangedEntity, TestSet> entry : changedClazzes.entrySet()) {
-
-               String changedEntity = entry.getKey().toString();
-               if (entry.getValue().getTests().size() > 0) {
-                  for (TestCase test : entry.getValue().getTests()) {
-                     Map<TestCase, TestcaseStatistic> commitStatistics = statistics.getStatistics().get(commit);
-                     TestcaseStatistic testcaseStatistic;
-                     if (commitStatistics != null) {
-                        testcaseStatistic = commitStatistics.get(test);
-                     } else {
-                        testcaseStatistic = null;
-                     }
-
-                     Changes commitChanges = changes.getCommitChanges(commit);
-                     Change change = commitChanges.getChange(test);
-
-                     double changeValue;
-                     if (change != null) {
-                        changeValue = change.getChangePercent();
-                     } else if (testcaseStatistic != null) {
-                        changeValue = 0.0;
-                     } else {
-                        changeValue = Double.NaN;
-                     }
-
-                     ChangeLine line = new ChangeLine(commit, changedEntity, test.toString(), changeValue);
-                     result.add(line);
-                  }
-               } else {
-                  ChangeLine line = new ChangeLine(commit, changedEntity, "none", Double.NaN);
-                  result.add(line);
-               }
-            }
+         
+         for (Map.Entry<String, CommitStaticSelection> commitEntry : selection.getCommits().entrySet()) {
+            String commit = commitEntry.getKey();
+            CommitStaticSelection commitStaticSelection = commitEntry.getValue();
+            addCommitData(result, commit, commitStaticSelection);
          }
       }
 
       return result;
+   }
+
+   private void addCommitData(List<ChangeLine> result, String commit, CommitStaticSelection commitStaticSelection) {
+      Map<ChangedEntity, TestSet> changedClazzes = commitStaticSelection.getChangedClazzes();
+      for (Map.Entry<ChangedEntity, TestSet> entry : changedClazzes.entrySet()) {
+
+         String changedEntity = entry.getKey().toString();
+         if (entry.getValue().getTests().size() > 0) {
+            for (TestCase test : entry.getValue().getTests()) {
+               TestcaseStatistic testcaseStatistic = getTestcaseStatistic(commit, test);
+
+               Changes commitChanges = changes.getCommitChanges(commit);
+               Change change = commitChanges.getChange(test);
+
+               double changeValue = getPrintableChangeValue(testcaseStatistic, change);
+
+               ChangeLine line = new ChangeLine(commit, changedEntity, test.toString(), changeValue);
+               result.add(line);
+            }
+         } else {
+            ChangeLine line = new ChangeLine(commit, changedEntity, "none", Double.NaN);
+            result.add(line);
+         }
+      }
+   }
+
+   private TestcaseStatistic getTestcaseStatistic(String commit, TestCase test) {
+      Map<TestCase, TestcaseStatistic> commitStatistics = statistics.getStatistics().get(commit);
+      TestcaseStatistic testcaseStatistic;
+      if (commitStatistics != null) {
+         testcaseStatistic = commitStatistics.get(test);
+      } else {
+         testcaseStatistic = null;
+      }
+      return testcaseStatistic;
+   }
+
+   private double getPrintableChangeValue(TestcaseStatistic testcaseStatistic, Change change) {
+      double changeValue;
+      if (change != null) {
+         changeValue = change.getChangePercent();
+      } else if (testcaseStatistic != null) {
+         changeValue = 0.0;
+      } else {
+         changeValue = Double.NaN;
+      }
+      return changeValue;
    }
 }
