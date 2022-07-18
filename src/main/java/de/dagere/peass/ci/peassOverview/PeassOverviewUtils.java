4ce575f5bb05d59d6b384e2e35b476448a8f6223
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-07-18 19:06:16 +0200

    Remove all non-trace selected tests

--- src/main/java/de/dagere/peass/ci/peassOverview/PeassOverviewUtils.java
+++ src/main/java/de/dagere/peass/ci/peassOverview/PeassOverviewUtils.java
@@ -13,20 +13,21 @@ import de.dagere.peass.dependency.persistence.StaticTestSelection;
 
 public class PeassOverviewUtils {
    public static void removeNotTraceSelectedTests(StaticTestSelection selection, ExecutionData data) {
-      String newestVersion = selection.getNewestCommit();
-      TestSet newestVersionTraceSelection = data.getCommits().get(newestVersion);
-      if (newestVersionTraceSelection != null) {
-         CommitStaticSelection versionStaticSelection = selection.getCommits().get(newestVersion);
-         if (versionStaticSelection != null) {
-            for (Map.Entry<ChangedEntity, TestSet> changedEntity : versionStaticSelection.getChangedClazzes().entrySet()) {
-               Set<TestCase> tests = new HashSet<>(changedEntity.getValue().getTests());
+      for (String commit : selection.getCommitNames()) {
+         TestSet commitTraceSelection = data.getCommits().get(commit);
+         if (commitTraceSelection != null) {
+            CommitStaticSelection versionStaticSelection = selection.getCommits().get(commit);
+            if (versionStaticSelection != null) {
+               for (Map.Entry<ChangedEntity, TestSet> changedEntity : versionStaticSelection.getChangedClazzes().entrySet()) {
+                  Set<TestCase> tests = new HashSet<>(changedEntity.getValue().getTests());
 
-               for (TestCase test : tests) {
-                  if (!newestVersionTraceSelection.getTests().contains(test)) {
-                     if (test.getMethod() != null) {
-                        changedEntity.getValue().removeTest(test.onlyClazz(), test.getMethod());
-                     } else {
-                        changedEntity.getValue().removeTest(test);
+                  for (TestCase test : tests) {
+                     if (!commitTraceSelection.getTests().contains(test)) {
+                        if (test.getMethod() != null) {
+                           changedEntity.getValue().removeTest(test.onlyClazz(), test.getMethod());
+                        } else {
+                           changedEntity.getValue().removeTest(test);
+                        }
                      }
                   }
                }
