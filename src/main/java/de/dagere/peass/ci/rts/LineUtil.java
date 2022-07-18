54671d67a57361ba89c4b5034dc4142ea9d678f3
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-07-18 17:02:46 +0200

    Add separate line for new commit

--- src/main/java/de/dagere/peass/ci/rts/LineUtil.java
+++ src/main/java/de/dagere/peass/ci/rts/LineUtil.java
@@ -7,7 +7,7 @@ import de.dagere.peass.dependency.analysis.data.ChangedEntity;
 
 public class LineUtil {
 
-   private static final int MAX_CHAR_COUNT = 80;
+   private static final int MAX_CHAR_COUNT = 120;
 
    public static List<String> createPrintable(String originalTestcase) {
       List<String> printableTestcase = new LinkedList<>();
