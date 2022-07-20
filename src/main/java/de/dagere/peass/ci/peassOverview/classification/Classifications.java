12bb4761a13f51cea1e7d2c1ce3d9a6edb0b7936
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-07-20 23:28:36 +0200

    First working implementation: Update classification by doUpdateClassification / doUpdateUnmeasured

--- src/main/java/de/dagere/peass/ci/peassOverview/classification/Classifications.java
+++ src/main/java/de/dagere/peass/ci/peassOverview/classification/Classifications.java
@@ -3,6 +3,8 @@ package de.dagere.peass.ci.peassOverview.classification;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Classifications {
    private Map<String, ClassifiedProject> projects = new LinkedHashMap<>();
 
@@ -12,5 +14,27 @@ public class Classifications {
 
    public void setProjects(Map<String, ClassifiedProject> projects) {
       this.projects = projects;
+   }
+
+   @JsonIgnore
+   public void setClassification(String project, String commit, String testcase, String classification) {
+      ClassifiedProject projectObject = projects.get(project);
+      if (projectObject == null) {
+         projectObject = new ClassifiedProject();
+         projects.put(project, projectObject);
+      }
+      TestcaseClassification testcaseClassifications = projectObject.getClassification(commit);
+      testcaseClassifications.setClassificationValue(testcase, classification);
+   }
+   
+   @JsonIgnore
+   public void setUnmeasuredClassification(String project, String commit, String testcase, String classification) {
+      ClassifiedProject projectObject = projects.get(project);
+      if (projectObject == null) {
+         projectObject = new ClassifiedProject();
+         projects.put(project, projectObject);
+      }
+      TestcaseClassification testcaseClassifications = projectObject.getUnmeasuredClassification(commit);
+      testcaseClassifications.setClassificationValue(testcase, classification);
    }
 }
