97b4fa9395f3de4c10a5d163bca0099503cdf3e8
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-07-24 19:32:17 +0200

    Derive the unmeasured tests

--- src/main/java/de/dagere/peass/ci/peassOverview/ProjectOverviewStatistic.java
+++ src/main/java/de/dagere/peass/ci/peassOverview/ProjectOverviewStatistic.java
@@ -4,12 +4,18 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import de.dagere.peass.analysis.changes.Change;
+import de.dagere.peass.analysis.changes.Changes;
 import de.dagere.peass.ci.peassOverview.classification.ClassifiedProject;
 import de.dagere.peass.ci.peassOverview.classification.TestcaseClassification;
+import de.dagere.peass.dependency.analysis.data.TestCase;
+import de.dagere.peass.dependency.persistence.CommitStaticSelection;
+import de.dagere.peass.measurement.statistics.data.TestcaseStatistic;
 
 public class ProjectOverviewStatistic {
    private int commitsWithSourceChange;
@@ -20,9 +26,9 @@ public class ProjectOverviewStatistic {
    private Map<String, Integer> categoryCommitCount = new LinkedHashMap<>();
 
    public ProjectOverviewStatistic() {
-      
+
    }
-   
+
    public int getCommitsWithSourceChange() {
       return commitsWithSourceChange;
    }
@@ -30,11 +36,11 @@ public class ProjectOverviewStatistic {
    public void setCommitsWithSourceChange(int commitsWithSourceChange) {
       this.commitsWithSourceChange = commitsWithSourceChange;
    }
-   
+
    public int getTestsWithSourceChange() {
       return testsWithSourceChange;
    }
-   
+
    public void setTestsWithSourceChange(int testsWithSourceChange) {
       this.testsWithSourceChange = testsWithSourceChange;
    }
@@ -46,7 +52,7 @@ public class ProjectOverviewStatistic {
    public void setCommitsWithChange(int commitWithChange) {
       this.commitsWithChange = commitWithChange;
    }
-   
+
    public int getTestsWithChange() {
       return testsWithChange;
    }
@@ -78,7 +84,7 @@ public class ProjectOverviewStatistic {
    public void setCategoryCommitCount(Map<String, Integer> categoryCommitCount) {
       this.categoryCommitCount = categoryCommitCount;
    }
-   
+
    public void increaseCategoryTestCount(String category) {
       Integer count = categoryTestCount.get(category);
       if (count == null) {
@@ -88,7 +94,7 @@ public class ProjectOverviewStatistic {
       }
       categoryTestCount.put(category, count);
    }
-   
+
    public void increaseCategoryCommitCount(String category) {
       Integer count = categoryCommitCount.get(category);
       if (count == null) {
@@ -98,44 +104,59 @@ public class ProjectOverviewStatistic {
       }
       categoryCommitCount.put(category, count);
    }
-   
+
    public static ProjectOverviewStatistic getFromClassification(String projectName, ProjectData projectData, ClassifiedProject classificationData) {
       ProjectOverviewStatistic statistic = new ProjectOverviewStatistic();
-      
+
       int commitCount = projectData.getStatistics().getStatistics().size();
       statistic.setCommitsWithSourceChange(commitCount);
       int tests = projectData.getStatistics().getTestCount();
       statistic.setTestsWithSourceChange(tests);
       statistic.setCommitsWithChange(projectData.getChanges().getCommitChanges().size());
       statistic.setTestsWithChange(projectData.getChanges().getChangeCount());
-//      statistic.setCommitsWithChange();
-      
+      // statistic.setCommitsWithChange();
+
       Set<String> commits = new LinkedHashSet<>();
       commits.addAll(classificationData.getChangeClassifications().keySet());
       commits.addAll(classificationData.getUnmeasuredClassifications().keySet());
-      
+
       for (String commit : commits) {
          Set<String> classificationsInThisCommit = new HashSet<>();
-         
+
          addChangeClassifications(classificationData, statistic, commit, classificationsInThisCommit);
          addUnmeasuredClassifications(classificationData, statistic, commit, classificationsInThisCommit);
-         
+
+         CommitStaticSelection commitSelection = projectData.getSelection().getCommits().get(commit);
+         System.out.println("commitChanges: " + commitSelection);
+         if (commitSelection != null) {
+            int unmeasured = statistic.getUnmeasuredTests();
+            Map<TestCase, TestcaseStatistic> commitStatistic = projectData.getStatistics().getStatistics().get(commit);
+            for (TestCase test : commitSelection.getTests().getTests()) {
+               if (commitStatistic == null) {
+                  unmeasured++;
+               } else if (commitStatistic.get(test) == null) {
+                  unmeasured++;
+               }
+            }
+            statistic.setUnmeasuredTests(unmeasured);
+         }
+
          for (String commitClassification : classificationsInThisCommit) {
             statistic.increaseCategoryCommitCount(commitClassification);
          }
       }
       return statistic;
    }
-   
+
    public static ProjectOverviewStatistic getSumStatistic(Collection<ProjectOverviewStatistic> statistics) {
       ProjectOverviewStatistic result = new ProjectOverviewStatistic();
-      
+
       for (ProjectOverviewStatistic statistic : statistics) {
          result.setCommitsWithChange(result.getCommitsWithChange() + statistic.getCommitsWithChange());
          result.setCommitsWithSourceChange(result.getCommitsWithSourceChange() + statistic.getCommitsWithSourceChange());
          result.setTestsWithChange(result.getTestsWithChange() + statistic.getTestsWithChange());
          result.setTestsWithSourceChange(result.getTestsWithSourceChange() + statistic.getTestsWithSourceChange());
-         
+
          for (String category : statistic.getCategoryCommitCount().keySet()) {
             Integer categoryCommitCount = result.getCategoryCommitCount().get(category);
             if (categoryCommitCount == null) {
@@ -143,7 +164,7 @@ public class ProjectOverviewStatistic {
             }
             categoryCommitCount += statistic.getCategoryCommitCount().get(category);
             result.getCategoryCommitCount().put(category, categoryCommitCount);
-            
+
             Integer categoryTestCount = result.getCategoryTestCount().get(category);
             if (categoryTestCount == null) {
                categoryTestCount = 0;
@@ -152,7 +173,7 @@ public class ProjectOverviewStatistic {
             result.getCategoryTestCount().put(category, categoryTestCount);
          }
       }
-      
+
       return result;
    }
 
