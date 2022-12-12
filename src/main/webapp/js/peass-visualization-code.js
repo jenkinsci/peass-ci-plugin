48f911b9c88921c0ca625f546afc146ce49fd8a3
DaGeRe <davidgeorg_reichelt@dagere.de>
2022-12-12 16:47:40 +0100

    Fix dashboard link when using jenkins

--- src/main/webapp/js/peass-visualization-code.js
+++ src/main/webapp/js/peass-visualization-code.js
@@ -292,7 +292,7 @@ function shownode(node) {
   var inspectLink = "";
   if (node.ess != -1) {
     if (typeof jenkins !== 'undefined') {
-      inspectLink = "<a href='"+treeData[0].call.replace("#", "_") +"_dashboard.html?ess="+node.ess+"&call=" + encodeURIComponent(node.call)+"' target='parent'>Inspect Node</a><br><br>";
+      inspectLink = "<a href='dashboard?ess="+node.ess+"&call=" + encodeURIComponent(node.call)+"' target='parent'>Inspect Node</a><br><br>";
     } else {
       var methodName = treeData[0].call.substr(treeData[0].call.indexOf("#")+1);
       inspectLink = "<a href='"+methodName +"_dashboard.html?ess="+node.ess+"&call=" + encodeURIComponent(node.call)+"' target='parent'>Inspect Node</a><br><br>";
